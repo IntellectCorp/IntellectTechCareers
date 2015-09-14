3b8e97868c0a9735d0e1cd19d9ae0d8e7aef8c4b
akankari <akankari@visa.com>
2015-09-14 16:49:48 +0530

    Dummy commit

---
+++ IntellectTechCareers/Controllers/LoginController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IntellectTechCareers.Controllers
+{
+    public class LoginController : Controller
+    {
+        //
+        // GET: /Login/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //[AllowAnonymous]
+        //public ActionResult Login(string returnUrl)
+        //{
+
+        //}
+    }
+}
