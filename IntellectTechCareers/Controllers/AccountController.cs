e4c29c180ea38a2f7b78e8fac0de73b9bd74897d
Ashish Kankaria <kankaria.ashish8790@gmail.com>
2015-09-14 18:04:03 +0530

    Adding action controller

---
+++ IntellectTechCareers/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IntellectTechCareers.Models;
+using IntellectTechCareers.Utils;
+
+namespace IntellectTechCareers.Controllers
+{
+    public class AccountController : Controller
+    {
+        //
+        // GET: /Login/
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult Login(LoginModel model, string returnUrl)
+        {
+            bool isValid = DBUtils.validateUser(model.UserName, model.Password);
+
+            if (!isValid)
+                return RedirectToAction("Login", "Account");
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+    }
+}
