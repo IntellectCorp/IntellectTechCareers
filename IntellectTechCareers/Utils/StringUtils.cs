25075470d3d26338cc2b75bbbc694c01ee7fa332
Kaushik Mahato <kkm.iitkgp@gmail.com>
2015-09-15 14:30:50 +0530

    Hash Function Implemented

---
+++ IntellectTechCareers/Utils/StringUtils.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace IntellectTechCareers.Utils
+{
+    public class StringUtils
+    {
+        public static string Reverse(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        public static string getMD5Hash(string input)
+        {
+            // given a string, byte array representation of that string
+            byte[] encodedPassword = new UTF8Encoding().GetBytes(input);
+
+            // need MD5 to calculate the hash
+            byte[] hash = ((HashAlgorithm)CryptoConfig.CreateFromName("MD5")).ComputeHash(encodedPassword);
+
+            // string representation (similar to UNIX format)
+            string encoded = BitConverter.ToString(hash);
+
+            return encoded;
+        }
+    }
+}
