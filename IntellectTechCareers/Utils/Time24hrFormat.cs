f33ad5d52cb9c29bea0b48824161d86b8b8b85c4
Kaushik Mahato <kkm.iitkgp@gmail.com>
2015-09-23 21:23:44 +0530

    InterView Schedule Working

---
+++ IntellectTechCareers/Utils/Time24hrFormat.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IntellectTechCareers.Utils
+{
+    public class Time24hrFormat
+    {
+        public Time24hrFormat()
+        {
+            Hour = 0;
+            Minute = 0;
+        }
+
+        public Time24hrFormat(string HourAndMinute)
+        {
+            string[] val = HourAndMinute.Split(':');
+            Hour = Int32.Parse(val[0]);
+            Minute = Int32.Parse(val[1]);
+        }
+
+        public int Hour { get; set; }
+        public int Minute { get; set; }
+
+        public override string ToString()
+        {
+            return Hour+":"+Minute;
+        }
+    }
+}
